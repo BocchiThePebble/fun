8a7c68fb68b4b79549adb581ebc2832d639c28d6
BocchiThePebble <anthonycn01130@gmail.come>
2025-03-06 18:06:02 -0500

    i did a dent amount

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,1 +1,19 @@
 alert("HELLO") 
+
+
+let age = 16
+  
+
+const birthYear = 2009;
+
+console.log("my birth year is", birthYear)
+
+console.log("my age is", age)
+
+if (age >= 18){
+    console.log('you are an adult');
+}
+
+else {
+    console.log('stay in school lil bro')
+}
