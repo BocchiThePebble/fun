47864db23f81def318f05f090a8953012d54d6c6
BocchiThePebble <anthonycn01130@gmail.come>
2025-03-25 17:49:37 -0400

    added matht

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -2,7 +2,11 @@ alert("HELLO")
 
 
 let age = 16
-  
+
+first_number = 1
+second_number = 6658695695986589658968968965
+total = first_number+second_number
+console.log(total)
 
 const birthYear = 2009;
 
@@ -10,8 +14,8 @@ console.log("my birth year is", birthYear)
 
 console.log("my age is", age)
 
-if (age >= 18){
-    console.log('you are an adult');
+if (age < 18 || age >30 ){
+    console.log('you are not a adult');
 }
 
 else {
