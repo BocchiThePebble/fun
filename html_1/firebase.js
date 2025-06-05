62a74f5bff2b22da965cf4a47c15df331b508837
coding328 <simrahk74@gmail.com>
2025-06-05 16:01:03 -0400

    fixed fire base login :)

---
+++ html_1/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC-G9XcoCE6itDSM1tB_q4W4LuxlooFy7M",
+    authDomain: "sigma-ab9fc.firebaseapp.com",
+    projectId: "sigma-ab9fc",
+    storageBucket: "sigma-ab9fc.firebasestorage.app",
+    messagingSenderId: "790510874863",
+    appId: "1:790510874863:web:cfd6c9cea4e886842feb1a",
+    measurementId: "G-QT7LDYZWW9"
+  };
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase.initializeApp(firebaseConfig);
+  firebase.auth();
+  firebase.firestore();
+  firebase.storage();
